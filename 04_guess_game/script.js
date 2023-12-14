bd7c9b737738b82fb3b2335db3c6956933040f78
Harsh2426 <harshpatel9811838836@gmail.com>
2023-12-14 22:45:53 +0530

    adding files

---
+++ 04_guess_game/script.js
@@ -0,0 +1,34 @@
+let guesses = document.querySelector(".guesses");
+let lastResult = document.querySelector(".lastResult");
+let lowOrHi = document.querySelector(".lowOrHi");
+let guessField = document.querySelector(".guessField");
+let max = 100;
+let min = 1;
+let prev = "";
+let chances = 10;
+
+let answer = Math.floor(Math.random() * (max - min + 1)) + min;
+
+let btn = document.querySelector(".guessSubmit");
+
+btn.addEventListener("click", (e) => {
+  e.preventDefault();
+  guesses.innerHTML = prev;
+  console.log(answer);
+  let enterVal = Number.parseInt(guessField.value);
+  if (chances <= 0) {
+    document.body.classList.add("lost");
+  } else {
+    if (enterVal === answer) {
+      document.body.classList.add("winner");
+      lowOrHi.innerHTML = "You enetered a correct number";
+    } else if (enterVal < answer) {
+      lowOrHi.innerHTML = "You enetered a smaller number";
+    } else {
+      lowOrHi.innerHTML = "You enetered a greater number";
+    }
+    prev = enterVal;
+    chances--;
+    lastResult.innerHTML = chances;
+  }
+});
